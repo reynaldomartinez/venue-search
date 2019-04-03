1ecb3df2ee93181fe1ba9f9c4e20fdc0e043c3ad
reynaldo <reynaldozig@hotmail.com>
2019-04-03 15:15:11 -0400

    fourth test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VenueListComponent } from './venue-list/venue-list.component';
 import { HomeComponent } from './home/home.component';
+import {TestiComponent} from './testi/testi.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'venues', component: VenueListComponent}
+  { path: 'venues', component: VenueListComponent},
+  { path: 'test', component: TestiComponent}
 ];
 
 @NgModule({
