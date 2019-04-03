3b85a7e402c6608a5be7bf0db23e5f7e5e84e542
reynaldo <reynaldozig@hotmail.com>
2019-04-03 15:57:32 -0400

    6th test

--- src/app/venue-list/venue-list.component.ts
+++ src/app/venue-list/venue-list.component.ts
@@ -18,17 +18,21 @@ export class VenueListComponent implements OnInit {
             this.location = position.coords;
           // let x = position.coords;
           // console.log(this.location);
-            this.venueService.getVenues(this.location).subscribe( (data): any => {
-                // this.venueDetails = data.response.groups[0].items;
-              this.venueDetails = [];
+            this.venueService.getVenues(this.location).subscribe( (data) => {
+                this.venueDetails = data.response.groups[0].items;
+                console.log(<Object>this.venueDetails);
+              // this.venueDetails = [];
               // this.venueDetails = data.response.groups[0].items;
-                console.log(this.venueDetails);
-                console.log(data);
+              //   console.log(this.venueDetails);
+              console.log(data);
+
           // console.log(this.venueDetails);
         });
       });
     } else {
         this.error = true;
+        console.log(this.error);
     }
   }
+
 }
